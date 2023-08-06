3e36e339552110a5608f22cb533f6f5da5cae4a5
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-08-06 15:52:22 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="hero"></div>
+      <h1 className="main-header">IP Address Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
