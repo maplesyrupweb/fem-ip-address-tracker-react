e3ef6f0eae8c46f078644c745841d12a35854237
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-08-11 00:16:14 -0700

    Add Bootstrap spinner

--- src/components/display-spinner/DisplaySpinner.js
+++ src/components/display-spinner/DisplaySpinner.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+import Spinner from 'react-bootstrap/Spinner';
+  
 
 import styles from './DisplaySpinner.module.css';
 
+/*
 const DisplaySpinner = () => {
-  return <FontAwesomeIcon icon={faSpinner} className={styles.spinner} />;
+  return <FontAwesomeIcon icon={faCircleNotch} className={styles.spinner} />;
+};
+*/
+
+const DisplaySpinner = () => {
+  return <Spinner animation="border" variant="secondary"className={styles.spinner} />
+  
+  ;
 };
 
 export default DisplaySpinner;
+<i class="fa-solid fa-circle-notch"></i>
